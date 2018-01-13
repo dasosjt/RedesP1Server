801ecc8a527293d7c3dd55fa4ebdba434dea822d
dasosjt <stdjca@gmail.com>
2018-01-13 01:16:05 -0600

    server emit change position

--- server.js
+++ server.js
@@ -6,32 +6,39 @@ const immutable = require('immutable')
 let port = process.env.PORT || 8080
 
 let globalState = immutable.fromJS({
-  clients: [{
-    id: 'Luisa',
-    position: {
-      x: 0,
-      y: 0
-    }
-  },{
-    id: 'Chan',
-    position: {
-      x: 100,
-      y: 100
-    }
-  }]
+  clients: []
 })
 
 server.listen(port)
 console.log('Server listening on port', port)
 
-io.on('connection', (client) => {
-  client.on('subscribeToState', (interval) => {
-    console.log('client', client.id,'is subscribing to state with interval', interval)
+io.on('connection', client => {
+  client.on('subscribeToState', interval => {
+    /*console.log('client', client.id,'is subscribing to state with interval', interval)*/
     
-    let updatedClients = globalState.get('clients').push(immutable.fromJS({ id: client.id, position: { x: 100, y: 100 } }))
+    let updatedClients = globalState.get('clients').push(immutable.fromJS({ id: client.id, position: { x: 0, y: 0 } }))
     globalState = globalState.set('clients', updatedClients)
         
     setInterval(() => { client.emit('state', globalState) }, interval)
+  })
+
+  client.on('emitPositionChange', change => {
+    /*console.log('client', client.id, 'is EPC', change)*/
+
+    let updatedClients = globalState.get('clients').map( c => c.get('id') === client.id ? immutable.fromJS({ id: client.id, position: change.position }) : c )
+
+    globalState = globalState.set('clients', updatedClients) 
+
+    client.emit('state', globalState)  
+  })
+
+  client.on('disconnect', () => {
+    /*console.log('client', client.id,'is disconnected')*/
+    let deleteClient = client.id
+    let updatedClients = globalState.get('clients').filterNot(client => client.get('id') === deleteClient)
+    globalState = globalState.set('clients', updatedClients)
+
+    client.emit('state', globalState)
   })
 })
 
