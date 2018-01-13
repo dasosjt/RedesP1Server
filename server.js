32a0ae61149b122b5f7d7adeeed87c9a3802a327
dasosjt <stdjca@gmail.com>
2018-01-12 23:36:49 -0600

    Single data structure to handle all clients positions on connect.

--- server.js
+++ server.js
@@ -2,18 +2,36 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-
+const immutable = require('immutable')
 let port = process.env.PORT || 8080
+
+let globalState = immutable.fromJS({
+  clients: [{
+    id: 'Luisa',
+    position: {
+      x: 0,
+      y: 0
+    }
+  },{
+    id: 'Chan',
+    position: {
+      x: 100,
+      y: 100
+    }
+  }]
+})
 
 server.listen(port)
 console.log('Server listening on port', port)
 
 io.on('connection', (client) => {
-  client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval)
-    setInterval(() => {
-      client.emit('timer', new Date())
-    }, interval)
+  client.on('subscribeToState', (interval) => {
+    console.log('client', client.id,'is subscribing to state with interval', interval)
+    
+    let updatedClients = globalState.get('clients').push(immutable.fromJS({ id: client.id, position: { x: 100, y: 100 } }))
+    globalState = globalState.set('clients', updatedClients)
+        
+    setInterval(() => { client.emit('state', globalState) }, interval)
   })
 })
 
