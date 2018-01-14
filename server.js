a671347c9d79a374b5165a4ae3d38b15072d8ff8
Bryan Chan <xchangip@gmail.com>
2018-01-13 20:01:21 -0600

    Moving squares

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const immutable = require('immutable')
+const randomColor = require('randomcolor')
 let port = process.env.PORT || 8080
 
 let globalState = immutable.fromJS({
@@ -16,7 +17,7 @@ io.on('connection', client => {
   client.on('subscribeToState', interval => {
     /*console.log('client', client.id,'is subscribing to state with interval', interval)*/
     
-    let updatedClients = globalState.get('clients').push(immutable.fromJS({ id: client.id, position: { x: 0, y: 0 } }))
+    let updatedClients = globalState.get('clients').push(immutable.fromJS({ id: client.id, position: { x: 0, y: 0 }, color: randomColor() }))
     globalState = globalState.set('clients', updatedClients)
         
     setInterval(() => { client.emit('state', globalState) }, interval)
@@ -26,7 +27,6 @@ io.on('connection', client => {
     /*console.log('client', client.id, 'is EPC', change)*/
 
     let updatedClients = globalState.get('clients').map( c => c.get('id') === client.id ? immutable.fromJS({ id: client.id, position: change.position }) : c )
-
     globalState = globalState.set('clients', updatedClients) 
 
     client.emit('state', globalState)  
